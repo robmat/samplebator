d4d02e0ff3d39f91fa91421d06c3e7b76d33d5fb
robbator <robbator@users.noreply.github.com>
2011-02-28 20:50:05 +0000

    Cache work.

--- bcm/src/com/bcm/DataCache.java
+++ bcm/src/com/bcm/DataCache.java
@@ -6,33 +6,21 @@ import java.util.Hashtable;
 import net.rim.device.api.system.DeviceInfo;
 
 public class DataCache implements IWaitableScreen {
-	private static final int DOWN_PROC = 1;
-	private static final int DOWN_SCEN = 2;
-	private static final int DOWN_ASSE = 3;
-	private int state = -1;
-	public Hashtable[] processes;
-	public Hashtable[] scenarios;
-	public Hashtable[] assets;
-	public Date processesDate;
-	public Date scenariosDate;
-	public Date assetsDate;
+	public Hashtable[] cache;
+	public Date cacheDate;
 	public IDataCacheAware dataCacheAware;
+	public String command;
 	
-	
-	public DataCache(IDataCacheAware dataCacheAware) {
+	public DataCache(IDataCacheAware dataCacheAware, String command) {
 		super();
 		this.dataCacheAware = dataCacheAware;
+		this.command = command;
 	}
 
 	public boolean fillInCache() {
 		try {
 			DataReceiver dr = new DataReceiver();
-			state = DOWN_PROC;
-			dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), DataCache.this, "getAllProcesses", null);
-			state = DOWN_ASSE;
-			dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), DataCache.this, "getAllAssets", null);
-			state = DOWN_SCEN;
-			dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), DataCache.this, "getAllScenarios", null);
+			dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), DataCache.this, command, null);
 		} catch (Exception e) {
 			log(e.getClass() + " " + e.getMessage());
 			return false;
@@ -41,20 +29,8 @@ public class DataCache implements IWaitableScreen {
 	}
 
 	public int callback(String msg) {
-		switch (state) {
-			case DOWN_PROC: {
-				processes = XMLUtils.getArrayItems(XMLUtils.parseXML(msg));
-				processesDate = new Date();
-			}
-			case DOWN_ASSE: {
-				assets = XMLUtils.getArrayItems(XMLUtils.parseXML(msg));
-				assetsDate = new Date();
-			}
-			case DOWN_SCEN: {
-				scenarios = XMLUtils.getArrayItems(XMLUtils.parseXML(msg));
-				scenariosDate = new Date();
-			}	
-		}
+		cache = XMLUtils.getArrayItems(XMLUtils.parseXML(msg));
+		cacheDate = new Date();
 		return 0;
 	}
 	public void startWaiting() {}
---
+++ bcm/src/com/bcm/NotifyTemplatesScreen.java
@@ -0,0 +1,96 @@
+package com.bcm;
+
+import java.util.Hashtable;
+
+import net.rim.device.api.system.Bitmap;
+import net.rim.device.api.system.DeviceInfo;
+import net.rim.device.api.ui.DrawStyle;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.Manager;
+import net.rim.device.api.ui.MenuItem;
+import net.rim.device.api.ui.UiApplication;
+import net.rim.device.api.ui.component.ButtonField;
+import net.rim.device.api.ui.component.Dialog;
+import net.rim.device.api.ui.component.LabelField;
+
+import org.w3c.dom.Document;
+
+public class NotifyTemplatesScreen extends CommonsForScreen implements IWaitableScreen {
+	private Hashtable[] items;
+	private ColouredListField clf;
+	private Dialog dialog;
+
+	public NotifyTemplatesScreen() {
+		setTitle(new LabelField(I18n.bundle.getString(BcmResource.notifyTemplatesLbl), DrawStyle.HCENTER | Field.USE_ALL_WIDTH));
+		init();
+		addMenuItem(new MenuItem(I18n.bundle.getString(BcmResource.refreshLbl), 0, 1) {
+			public void run() {
+				UiApplication.getUiApplication().popScreen(NotifyTemplatesScreen.this);
+				UiApplication.getUiApplication().pushScreen(new NotifyTemplatesScreen());
+			}
+		});
+	}
+
+	private void init() {
+		final DataReceiver dr = new DataReceiver();
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), NotifyTemplatesScreen.this, "getAllNotifyTemplates", null);
+				} catch (Exception e) {
+					NotifyTemplatesScreen.this.errorDialog(e.getClass().getName() + " " + e.getMessage());
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+	public void log(String str) {
+	}
+	protected boolean navigationClick(int arg0, int arg1) {
+		if (clf != null) {
+			int i = clf.getSelectedIndex();
+			UiApplication.getUiApplication().pushScreen(new NewNotificationScreen(items[i]));
+		}
+		return true;
+	}
+
+	public void startWaiting() {
+		dialog = new Dialog(Dialog.D_OK, I18n.bundle.getString(BcmResource.loadDataLbl) + "...", 0, Bitmap.getPredefinedBitmap(Bitmap.HOURGLASS), Field.FIELD_HCENTER);
+		try {
+			ButtonField b = (ButtonField) ((Manager) ((Manager) dialog.getField(1)).getField(1)).getField(0);
+			b.setLabel(I18n.bundle.getString(BcmResource.cancelLbl));
+		} catch (Exception e) {
+			NotifyTemplatesScreen.this.errorDialog(e.getClass().getName() + " " + e.getMessage());
+			e.printStackTrace();
+		}
+		dialog.show();
+	}
+
+	public void stopWaiting() {
+		if (dialog != null && dialog.isDisplayed()) {
+			dialog.close();
+		}
+	}
+
+	public int callback(String msg) {
+		if (msg != null && msg instanceof String) {
+			Document doc = null;
+			doc = XMLUtils.parseXML(msg);
+			items = XMLUtils.getArrayItems(doc);
+			if (areAnyItemsThere()) {
+				final String[][] itemStrArr = new String[items.length][];
+				for (int i = 0; i < items.length; i++) {
+					String name = (String) items[i].get("Name");
+					String desc = (String) items[i].get("Desc");
+					itemStrArr[i] = new String[] { name, desc };
+				}
+				CellularLabelfield lf = new CellularLabelfield(new String[] { I18n.bundle.getString(BcmResource.nameLbl), I18n.bundle.getString(BcmResource.statusLbl) }, new int[] { 55, 45 }, 0, true);
+				add(lf);
+				clf = new ColouredListField(itemStrArr.length, new int[] { 55, 45 }, itemStrArr);
+				clf.set(new String[itemStrArr.length]);
+				add(clf);
+			}
+		}
+		return 0;
+	}
+}
