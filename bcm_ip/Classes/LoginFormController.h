24ddc2a6560822dbfc3c912efceeea15f7504995
robbator <robbator@users.noreply.github.com>
2011-03-05 07:38:39 +0000

    I18n

---
+++ bcm_ip/Classes/LoginFormController.h
@@ -0,0 +1,16 @@
+//
+//  LoginFormController.h
+//  bcm_ip
+//
+//  Created by User on 3/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LoginFormController : UIViewController {
+
+}
+
+@end
