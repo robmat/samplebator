077bbf8363aa625ea2b9b16042c2f9f991072009
robbator@gmail.com <robbator@gmail.com@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-03-05 06:56:59 +0000

    Start point for bcm_ip

---
+++ trunk/bcm_ip/Classes/bcm_ipAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  bcm_ipAppDelegate.h
+//  bcm_ip
+//
+//  Created by User on 3/4/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bcm_ipAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
