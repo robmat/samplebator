ae1f788c37bcdb6a2f246266a76eca33db24b621
robbator@gmail.com <robbator@gmail.com@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-03-06 11:49:46 +0000

    Login done, dictionary started.

---
+++ trunk/bcm_ip/Classes/Dictionary.h
@@ -0,0 +1,18 @@
+//
+//  Dictionary.h
+//  bcm_ip
+//
+//  Created by User on 3/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Dictionary : NSObject {
+	
+}
+
++ (void) initDictionary;
+
+@end
