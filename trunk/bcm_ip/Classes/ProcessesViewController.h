4635e367d020eca7b46a66ba5a8442c2064299c4
robbator@gmail.com <robbator@gmail.com@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-03-10 20:20:34 +0000

    Processes screen work start.

---
+++ trunk/bcm_ip/Classes/ProcessesViewController.h
@@ -0,0 +1,17 @@
+//
+//  ProcessesViewController.h
+//  bcm_ip
+//
+//  Created by User on 3/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HttpRequestWrapper.h"
+
+@interface ProcessesViewController : UITableViewController {
+	HttpRequestWrapper* httpRequest;
+	NSMutableArray* processesArray;
+}
+
+@end
