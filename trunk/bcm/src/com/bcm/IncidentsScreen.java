2093e2f997d8aa2ebcc6762be8afced46ea02cec
robbator <robbator@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-03-20 15:29:34 +0000

    Bug fix: incidents did not display.

--- trunk/bcm/src/com/bcm/IncidentsScreen.java
+++ trunk/bcm/src/com/bcm/IncidentsScreen.java
@@ -16,79 +16,78 @@ import net.rim.device.api.ui.component.LabelField;
 import org.w3c.dom.Document;
 
 public class IncidentsScreen extends CommonsForScreen implements IWaitableScreen, INavListener {
-	public Dialog dialog;
-	private Hashtable[] items;
+        public Dialog dialog;
 
-	public IncidentsScreen() {
-		setTitle(new LabelField(I18n.bundle.getString(BcmResource.incidentsLbl), DrawStyle.HCENTER | Field.USE_ALL_WIDTH));
-		init();
-		addMenuItem(new MenuItem(I18n.bundle.getString(BcmResource.refreshLbl), 0, 1) {
-			public void run() {
-				UiApplication.getUiApplication().popScreen(IncidentsScreen.this);
-				UiApplication.getUiApplication().pushScreen(new IncidentsScreen());
-			}
-		});
-	}
-	public void log(String str) {
-	}
-	public void init() {
-		final DataReceiver dr = new DataReceiver();
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), IncidentsScreen.this, "getAllIncidents", null);
-				} catch (Exception e) {
-					IncidentsScreen.this.errorDialog(e.getClass().getName() + " " + e.getMessage());
-					e.printStackTrace();
-				}
-			}
-		}).start();
-	}
+        public IncidentsScreen() {
+                setTitle(new LabelField(I18n.bundle.getString(BcmResource.incidentsLbl), DrawStyle.HCENTER | Field.USE_ALL_WIDTH));
+                init();
+                addMenuItem(new MenuItem(I18n.bundle.getString(BcmResource.refreshLbl), 0, 1) {
+                        public void run() {
+                                UiApplication.getUiApplication().popScreen(IncidentsScreen.this);
+                                UiApplication.getUiApplication().pushScreen(new IncidentsScreen());
+                        }
+                });
+        }
+        public void log(String str) {
+        }
+        public void init() {
+                final DataReceiver dr = new DataReceiver();
+                new Thread(new Runnable() {
+                        public void run() {
+                                try {
+                                        dr.getAllData(EntryPoint.authUser, EntryPoint.authPass, DeviceInfo.getDeviceId(), IncidentsScreen.this, "getAllIncidents", null);
+                                } catch (Exception e) {
+                                        IncidentsScreen.this.errorDialog(e.getClass().getName() + " " + e.getMessage());
+                                        e.printStackTrace();
+                                }
+                        }
+                }).start();
+        }
 
-	public int callback(String msg) {
-		if (msg != null && msg instanceof String) {
-			Document doc = null;
-			doc = XMLUtils.parseXML(msg);
-			items = XMLUtils.getArrayItems(doc);
-			if (areAnyItemsThere()) {
-				for (int i = 0; i < items.length; i++) {
-					String name = (String) items[i].get("SequenceName");
-					String time = (String) items[i].get("IncidentTime");
-					MultiRowLabelField mrlf = new MultiRowLabelField(new String[] { name, time }, Field.FOCUSABLE | Field.USE_ALL_WIDTH);
-					mrlf.setArbitraryData(items[i]);
-					mrlf.setNavListener(this);
-					add(mrlf);
-				}
-			}
-		}
-		return 0;
-	}
+        public int callback(String msg) {
+                if (msg != null && msg instanceof String) {
+                        Document doc = null;
+                        doc = XMLUtils.parseXML(msg);
+                        items = XMLUtils.getArrayItems(doc);
+                        if (areAnyItemsThere()) {
+                                for (int i = 0; i < items.length; i++) {
+                                        String name = (String) items[i].get("SequenceName");
+                                        String time = (String) items[i].get("IncidentTime");
+                                        MultiRowLabelField mrlf = new MultiRowLabelField(new String[] { name, time }, Field.FOCUSABLE | Field.USE_ALL_WIDTH);
+                                        mrlf.setArbitraryData(items[i]);
+                                        mrlf.setNavListener(this);
+                                        add(mrlf);
+                                }
+                        }
+                }
+                return 0;
+        }
 
-	public boolean onSavePrompt() {
-		return true;
-	}
+        public boolean onSavePrompt() {
+                return true;
+        }
 
-	public void startWaiting() {
-		dialog = new Dialog(Dialog.D_OK, I18n.bundle.getString(BcmResource.loadDataLbl) + "...", 0, Bitmap.getPredefinedBitmap(Bitmap.HOURGLASS), Field.FIELD_HCENTER);
-		try {
-			ButtonField b = (ButtonField) ((Manager) ((Manager) dialog.getField(1)).getField(1)).getField(0);
-			b.setLabel(I18n.bundle.getString(BcmResource.cancelLbl));
-		} catch (Exception e) {
-			IncidentsScreen.this.errorDialog(e.getClass().getName() + " " + e.getMessage());
-			e.printStackTrace();
-		}
-		dialog.show();
-	}
+        public void startWaiting() {
+                dialog = new Dialog(Dialog.D_OK, I18n.bundle.getString(BcmResource.loadDataLbl) + "...", 0, Bitmap.getPredefinedBitmap(Bitmap.HOURGLASS), Field.FIELD_HCENTER);
+                try {
+                        ButtonField b = (ButtonField) ((Manager) ((Manager) dialog.getField(1)).getField(1)).getField(0);
+                        b.setLabel(I18n.bundle.getString(BcmResource.cancelLbl));
+                } catch (Exception e) {
+                        IncidentsScreen.this.errorDialog(e.getClass().getName() + " " + e.getMessage());
+                        e.printStackTrace();
+                }
+                dialog.show();
+        }
 
-	public void stopWaiting() {
-		if (dialog != null && dialog.isDisplayed()) {
-			dialog.close();
-		}
-	}
+        public void stopWaiting() {
+                if (dialog != null && dialog.isDisplayed()) {
+                        dialog.close();
+                }
+        }
 
-	public void navClcik(Field f) {
-		MultiRowLabelField mrlf = (MultiRowLabelField) f;
-		Hashtable item = (Hashtable) mrlf.getArbitraryData();
-		UiApplication.getUiApplication().pushScreen(new TasksScreen(TasksScreen.INCIDENT_TASKS, (String) item.get("Id"), (String) item.get("SequenceName")));
-	}
+        public void navClcik(Field f) {
+                MultiRowLabelField mrlf = (MultiRowLabelField) f;
+                Hashtable item = (Hashtable) mrlf.getArbitraryData();
+                UiApplication.getUiApplication().pushScreen(new TasksScreen(TasksScreen.INCIDENT_TASKS, (String) item.get("Id"), (String) item.get("SequenceName")));
+        }
 }
