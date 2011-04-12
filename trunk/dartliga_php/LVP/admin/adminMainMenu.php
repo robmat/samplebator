c2eb92f2b967eb31cb3c9539a18d6a842f225bbc
robbator@gmail.com <robbator@gmail.com@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-04-12 08:35:47 +0000

    Admin panel start.

--- trunk/dartliga_php/LVP/admin/adminMainMenu.php
+++ trunk/dartliga_php/LVP/admin/adminMainMenu.php
@@ -1,3 +1,136 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta name="generator" content=
+  "HTML Tidy for Linux/x86 (vers 11 February 2007), see www.w3.org" />
+
+  <title>LSDB System</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=us-ascii" />
+  <meta http-equiv="EXPIRES" content="0" />
+  <meta name="RESOURCE-TYPE" content="DOCUMENT" />
+  <meta name="DISTRIBUTION" content="GLOBAL" />
+  <meta name="AUTHOR" content="Hristovski" />
+  <meta name="COPYRIGHT" content="Copyright (c) 2002-08 by Austrian Darts Federation (Hristovski)" />
+  <meta name="KEYWORDS" content="League System,Rechenzentrum,&#214;DV,OEDSO,WDF,Austria" />
+  <meta name="DESCRIPTION" content="Liga System Application Austrian Darts Federation &#214;DV" />
+  <meta name="ROBOTS" content="INDEX, FOLLOW" />
+  <meta name="GENERATOR" content="LSDB System 4a" />
+  <script language="JavaScript" src="../code/jquery-1.2.6.js" type="text/javascript"></script>
+  <script language="JavaScript" src="../code/wz_jsgraphics.js" type="text/javascript"></script>
+  <script language="JavaScript" src="../code/lsdbfunc.js" type="text/javascript"></script>
+  <link rel="StyleSheet" href="../theme/Lite/style.css" type="text/css" />
+</head>
+
+<body class="lsdb">
+  <table class="fmain">
+    <tr>
+      <td class="f1"></td>
+
+      <td class="f2"><a href="../dso_user.php"><img src="../images/dblogo.png" border="0" /></a></td>
+
+      <td class="f3">
+        <table>
+          <tr>
+            <td></td>
+
+            <td></td>
+
+            <td class="redbox">DDSVEV - Datenzentrum - Version 4b</td>
+
+            <td></td>
+          </tr>
+        </table>
+      </td>
+
+      <td class="f4"></td>
+    </tr>
+
+    <tr>
+      <td></td>
+
+      <td colspan="2">
+        <div id="navi" class="navi"></div>
+      </td>
+
+      <td></td>
+    </tr>
+  </table>
+
+  <table class="fmain">
+    <tr>
+      <td class="f5"></td>
+
+      <td class="f6" id="leftpane" name="leftpane" valign="top"></td>
+
+      <td class="f7" id="mainpane" name="mainpane" valign="top">
+        <div style="height:5px">
+          &nbsp;
+        </div>
+
+        <h3 id="pagetitle"></h3>
+
+        <div id="pagetabs"></div>
+
+        <div id="maincontent">
+          <table class="box">
+            <tr>
+              <td>
+                <table>
+					<tr><td></td></tr>
+				</table>
+              </td>
+			  <td>
+                
+              </td>
+            </tr>
+			 <tr>
+              <td>
+                
+              </td>
+			  <td>
+                
+              </td>
+            </tr>
+			 <tr>
+              <td>
+                
+              </td>
+			  <td>
+                
+              </td>
+            </tr>
+          </table>
+
+          <div id="pinbox"></div>
+        </div>
+
+        <div id="bottomcontent"></div><br />
+
+        <div id="debug"></div>
+      </td>
+
+      <td></td>
+    </tr>
+
+    <tr>
+      <td></td>
+
+      <td></td>
+
+      <td></td>
+
+      <td></td>
+    </tr>
+  </table>
+
+  <div id="foot">
+    <center>
+      <font class="tiny">&#169; Hristovski 2003-2008 <a href="">Impressum</a></font>
+    </center>
+  </div>
+</body>
+</html>
+
+
 <?php 
 
 require_once('mainfile.php');
