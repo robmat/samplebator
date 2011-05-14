6e97bdc06baf9c19bcfafbdfa98cabb08c787fab
robbator <robbator@users.noreply.github.com>
2011-05-14 10:16:27 +0000

    Made a custom pathway cell in line with design.

---
+++ smart_gp_ip/Classes/PathwayCell.h
@@ -0,0 +1,20 @@
+//
+//  PathwayCell.h
+//  smart_gp_ip
+//
+//  Created by User on 5/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PathwayCell : UITableViewCell {
+	IBOutlet UILabel* label;
+	IBOutlet UILabel* detailLabel;
+}
+
+@property (nonatomic, retain) UILabel* label;
+@property (nonatomic, retain) UILabel* detailLabel;
+
+@end
