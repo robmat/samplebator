520a71eb7775613f159887694330b6fe237caa6e
robbator <robbator@users.noreply.github.com>
2011-05-21 10:55:32 +0000

    Fixes in admin rights page.
    3) Liga Gruppe Rechte Zuweisung
     
    - I can not pick up Admin user for changing something
    - Better sort list by> Admin User Name
    - in list I dont see under Liga Name year of  Liga name for i.e. standing 5 times Dillenburg B1 Liga. Something must be wrong there.
    For example >
    53    keving    Wetzlar C1 Liga    Edit Teams    1
    
    keving have rights for
    2011 Dbg 1 Bezirks Liga Saison I/11
    
    3.1) New Admin Liga
    
    - Right Name> this name is the same like the username you can select in Chekbox.
     It should not be allowed to give an extra Right name. Because in list I must see the username and which right they have in which classes for delete rihts or give new rights.

--- dartliga_php/LVP/admin_division_rights_to_users.php
+++ dartliga_php/LVP/admin_division_rights_to_users.php
@@ -10,20 +10,22 @@ global $usertoken;
 # List all admin liga users
 function admin_liga_list() {
 	global $dbi;
-	$sql =  'SELECT a.id, a.auname, e.evname, c.acdesc, a.aevactive, a.acomment '.
-			'FROM tbladminliga a, tblevent e, tligaaccesscode c '.
-			'WHERE a.aevcode_id = e.id '.
+	$sql =  'SELECT a.id, t.uname, e.evname, c.acdesc, a.aevactive, a.acomment '.
+			'FROM tbladminliga a, tblevent e, tligaaccesscode c, tuser t '.
+			'WHERE a.aevcode_id = e.id AND t.id = a.auid_id '.
 			'AND a.access_id = c.access_id '.
-			'ORDER BY a.id';
+			'ORDER BY t.uname';
 	$query_result = sql_query( $sql, $dbi );
 	
 	# Table header
-	$ret = '<table><tr><td class="thead">Id</td><td class="thead">Admin user name</td><td class="thead">Liga name</td>';
-	$ret = $ret.'<td class="thead">Access</td><td class="thead">Active</td><td class="thead">Comment</td><td class="thead">Del.</td></tr>';
+	$ret = '<table><tr><td class="thead">Id</td><td class="thead">Admin user name</td><td class="thead">Liga name</td><td class="thead">Access</td>';
+	$ret = $ret.'<td class="thead">Active</td><td class="thead">Comment</td><td class="thead">Del.</td><td class="thead">Edit</td></tr>';
 	
 	while ( list( $id, $auname, $evname, $acdesc, $aevactive, $acomment ) = sql_fetch_row( $query_result, $dbi ) ) {
 		$ret = $ret.'<tr><td>'.$id.'</td><td>'.$auname.'</td><td>'.$evname.'</td><td>'.$acdesc.'</td><td>'.$aevactive.'</td><td>'.$acomment.'</td>';
-		$ret = $ret.'<td><img src="images/del_icon.png" style="cursor: pointer;" onclick="deleteAdminLiga('.$id.');" /></td></tr>';
+		$ret = $ret.'<td><img src="images/del_icon.png" style="cursor: pointer;" onclick="deleteAdminLiga('.$id.');" /></td>';
+		$location_path = 'admin_division_rights_to_users.php?op=new_admin_liga&aid='.$id;
+		$ret = $ret.'<td><img src="images/edit24.png" style="cursor: pointer;" onclick="window.location.href = \''.$location_path.'\'" /></td></tr>';
 	}
 	
 	return $ret.'</table>';
@@ -31,6 +33,7 @@ function admin_liga_list() {
 
 # New admin user form
 function new_admin_liga() {
+	global $dbi;	
 	if ( isset( $_REQUEST['name'] ) ) { $name=strip_tags( $_REQUEST['name'] ); }
 	if ( isset( $_REQUEST['uname'] ) ) { $uname=strip_tags( $_REQUEST['uname'] ); }
 	if ( isset( $_REQUEST['liganame'] ) ) { $liganame=strip_tags( $_REQUEST['liganame'] ); }
@@ -38,12 +41,18 @@ function new_admin_liga() {
 	if ( isset( $_REQUEST['active'] ) ) { $active=strip_tags( $_REQUEST['active'] ); }
 	if ( isset( $_REQUEST['comment'] ) ) { $comment=strip_tags( $_REQUEST['comment'] ); }
 	
-	global $dbi;
-	$user_query_result = sql_query( 'SELECT u.id, u.uname FROM tuser u', $dbi );
-	$liga_query_result = sql_query( 'SELECT e.id, e.evname FROM tblevent e', $dbi );
+	if ( isset( $_REQUEST['aid'] ) && !empty( $_REQUEST['aid'] ) ) {
+		$aid = $_REQUEST['aid'];
+		$sql = 'SELECT a.access_id, a.aevactive, a.auid_id, a.cre_date, a.aevcode_id, a.acomment FROM tbladminliga a WHERE id = '.$aid;
+		$admin_liga_result = sql_query( $sql, $dbi );
+		list( $accesstype, $aevactive, $uname, $cre_date, $liganame, $comment ) = sql_fetch_row( $admin_liga_result, $dbi );
+	}
+	
+	$user_query_result = sql_query( 'SELECT u.id, u.uname FROM tuser u ORDER BY uname', $dbi );
+	$liga_query_result = sql_query( 'SELECT e.id, e.evname, e.evyear FROM tblevent e ORDER BY evname', $dbi );
 	$accs_query_result = sql_query( 'SELECT a.access_id, a.acdesc FROM tligaaccesscode a', $dbi );
 	
-	$ret = '<form action="admin_division_rights_to_users.php?op=new_liga_user_right_creation" method="post">';
+	$ret = '<form action="admin_division_rights_to_users.php?op=new_liga_user_right_creation&aid='.$aid.'" method="post">';
 	$ret = $ret.'<table><tr><td>User name:</td><td>';
 	$ret = $ret.'<select id="uname" name="uname">';
 	while ( list( $id, $name ) = sql_fetch_row( $user_query_result, $dbi ) ) {
@@ -52,9 +61,9 @@ function new_admin_liga() {
 	}
 	$ret = $ret.'</select></td></tr><tr><td>League:</td><td>';
 	$ret = $ret.'<select id="liganame" name="liganame">';
-	while ( list( $id, $name ) = sql_fetch_row( $liga_query_result, $dbi ) ) {
+	while ( list( $id, $name, $evyear ) = sql_fetch_row( $liga_query_result, $dbi ) ) {
 		$selected = strcmp( $liganame, $id ) == 0 ? 'selected="selected"' : '';
-		$ret = $ret.'<option '.$selected.' value="'.$id.'">'.$name.'</option>';
+		$ret = $ret.'<option '.$selected.' value="'.$id.'">'.$name.' '.$evyear.'</option>';
 	}
 	$ret = $ret.'</select></td></tr><tr><td>Access type:</td><td>';
 	$ret = $ret.'<select id="accesstype" name="accesstype">';
@@ -64,16 +73,14 @@ function new_admin_liga() {
 	}
 	$ret = $ret.'</select></td></tr><tr><td>Active:</td><td>';
 	$ret = $ret.'<select id="active" name="active">';
-	$ret = $ret.'<option value="1">Yes</option>';
-	$ret = $ret.'<option value="0">No</option></select>';
+	$ret = $ret.'<option value="1" '.( $aevactive == 1 ? 'selected' : '' ).'>Yes</option>';
+	$ret = $ret.'<option value="0" '.( $aevactive == 0 ? 'selected' : '' ).'>No</option></select>';
 	$ret = $ret.'</td></tr><tr><td>Comment:</td><td>';
 	$ret = $ret._input( 1, 'comment', $comment, 50, 50 );
 	$ret = $ret.'</td></tr><tr><td>Create date:</td><td>';
-	$ret = $ret.date('l jS \of F Y h:i:s A');
-	$ret = $ret.'</td></tr><tr><td>Right name:</td><td>';
-	$ret = $ret._input( 1, 'name', $name, 50, 50 );
+	$ret = $ret.( empty( $aid ) ? date('l jS \of F Y h:i:s A') : $cre_date );
 	$ret = $ret.'</td></tr><tr><td></td><td>';
-	$ret = $ret._button( 'Create right', '', 'admin_division_rights_to_users.php?op=new_liga_user_right_creation' );
+	$ret = $ret._button( empty( $aid ) ? 'Create right' : 'Edit right', '', 'admin_division_rights_to_users.php?op=new_liga_user_right_creation' );
 	$ret = $ret.'</td></tr></table></form>';
 	return $ret;
 }
@@ -96,12 +103,11 @@ function new_liga_user_right_creation() {
 	global $dbi;
 
 	$ret = '<div style="color: red; padding: 5px;">';
-	if ( empty( $_REQUEST['name'] ) ) { $ret = $ret.'Name required!<br/>'; }
-	if ( empty( $_REQUEST['active'] ) ) { $ret = $ret.'Active has to have a value of 0 or 1!<br/>'; }
-	if ( $active != 0 && $active != 1 ) { $ret = $ret.'Active has to have a value of 0 or 1!<br/>'; }
-	$ret = $ret.'</div>';
+	$ret = $ret.'</div>'; // no validation whatsoever
+	
 	
 	if ( strcmp( $ret, '<div style="color: red; padding: 5px;"></div>' )  == 0 ) { # valid
+		$aid = $_REQUEST['aid'];
 		$name = $_REQUEST['name'];
 		$uname = $_REQUEST['uname'];
 		$liganame = $_REQUEST['liganame'];
@@ -110,14 +116,18 @@ function new_liga_user_right_creation() {
 		$comment = $_REQUEST['comment'];
 		$sql = 'INSERT INTO tbladminliga (id, version, auname, access_id, aevactive, auid_id, cre_date, aevcode_id, acomment) ';
 		$sql = $sql.'VALUES (NULL, "0", "'.$name.'", "'.$accesstype.'", '.$active.', '.$uname.', "'.date("Y-m-d H:i:s").'", '.$liganame.', "'.$comment.'")';
-		$ret = $ret.$sql;
+		
+		if ( empty( $aid ) ) {
+			$sql = 'UPDATE tbladminliga SET auid_id = '.$uname.', access_id = '.$accesstype.', aevactive = '.$active.', aevcode_id = '.$liganame.', acomment = '.$comment.' WHERE id = '.$aid;
+		}
+		
 		$insert_result = sql_query( $sql, $dbi );
 		
 		if ($insert_result == TRUE) {
 			$ret = 'Creation successful!<br/><br/>';
 			$ret = $ret.admin_liga_list();		
 		} else {
-			$ret = $ret.'<div style="color: red;">Creation of a user liga right failed for unknown reasons!</div>';		
+			$ret = $ret.'<div style="color: red;">Creation/editing of a user liga right failed for unknown reasons!</div>';		
 		}
 	} else {
 		$ret = $ret.new_admin_liga();
