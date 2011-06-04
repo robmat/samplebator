f841175e5b8447757b50ff895a5cf4587b88fad2
robbator <robbator@users.noreply.github.com>
2011-06-04 16:53:45 +0000

    Pathway cell restyled.

--- smart_gp_ip/Classes/PathwayCell.m
+++ smart_gp_ip/Classes/PathwayCell.m
@@ -7,11 +7,11 @@
 //
 
 #import "PathwayCell.h"
-
+#import "WebViewController.h"
 
 @implementation PathwayCell
 
-@synthesize label, detailLabel, phoneBtn, urlBtn;
+@synthesize label, detailLabel, phoneBtn, urlBtn, navController;
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
@@ -22,11 +22,27 @@
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
 }
+- (IBAction) urlAction: (id) sender {
+	WebViewController* wvc = [[WebViewController alloc] initWithNibName:nil bundle:nil];
+	NSString* urlStr = ((UIButton*) sender).titleLabel.text;
+	if ([urlStr rangeOfString:@"http://"].location != 0) {
+		urlStr = [NSString stringWithFormat:@"%@%@", @"http://", urlStr];
+	}
+	wvc.url = urlStr;
+	[self.navController pushViewController:wvc animated:YES];
+	[wvc release];
+}
+- (IBAction) phoneAction: (id) sender {
+	NSString* telNo = [NSString stringWithFormat:@"%@%@", @"tel:", ((UIButton*) sender).titleLabel.text];
+	NSURL *phoneNumberURL = [NSURL URLWithString:telNo];
+	[[UIApplication sharedApplication] openURL:phoneNumberURL];
+}
 - (void)dealloc {
 	[label release];
 	[detailLabel release];
 	[phoneBtn release];
 	[urlBtn release];
+	[navController release];
     [super dealloc];
 }
 
--- smart_gp_ip/Classes/TableViewController.m
+++ smart_gp_ip/Classes/TableViewController.m
@@ -25,9 +25,9 @@
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     NSDictionary* dataDict = [dataArray objectAtIndex: indexPath.row];
-    static NSString *CellIdentifier = @"PathwayCell";
+    //static NSString *CellIdentifier = @"PathwayCell";
     
-    PathwayCell *cell = (PathwayCell*) [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    PathwayCell *cell = nil;//(PathwayCell*) [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
 		
         NSArray *topLevelObjects = [[NSBundle mainBundle] loadNibNamed:@"PathwayCell" owner:self options:nil];
@@ -51,22 +51,28 @@
 	NSString* phone = [dataDict objectForKey:@"phone"] == nil ? @"" : [dataDict objectForKey:@"phone"];
 	NSString* website = [dataDict objectForKey:@"website"] == nil ? @"" : [dataDict objectForKey:@"website"];
 	NSString* addressStr = [NSString stringWithFormat:@"%@ %@ %@ %@", address, address2, city, postcode];
-	cell.detailLabel.text = [addressStr stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	addressStr = [addressStr stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 	
+	int moveFactor = 11;
 	if (website != nil && ![website isEqualToString:@""]) {
-		cell.urlBtn.titleLabel.text = website;
+		[cell.urlBtn setTitle:website forState: UIControlStateNormal];
+		[self clipButtonToItsTitleWidth:cell.urlBtn];	
 	} else {
-	
+		[self moveDownView:cell.label byPixels:[NSNumber numberWithInt:moveFactor]];
+		[self moveDownView:cell.detailLabel byPixels:[NSNumber numberWithInt:moveFactor]];
+		[self moveDownView:cell.phoneBtn byPixels:[NSNumber numberWithInt:moveFactor]];
 	}
 	if (phone != nil && ![phone isEqualToString:@""]) {
-		cell.phoneBtn.titleLabel.text = phone;
+		[cell.phoneBtn setTitle:phone forState: UIControlStateNormal];
+		[self clipButtonToItsTitleWidth:cell.phoneBtn];	
 	} else {
-		
+		[self moveDownView:cell.label byPixels:[NSNumber numberWithInt:moveFactor]];
+		[self moveDownView:cell.detailLabel byPixels:[NSNumber numberWithInt:moveFactor]];
 	}
-	if ([cell.detailLabel.text isEqualToString:@""]) {
-		cell.detailLabel.hidden = YES;
+	if (addressStr != nil && ![addressStr isEqualToString:@""]) {
+		cell.detailLabel.text = addressStr;
 	} else {
-		
+		[self moveDownView:cell.label byPixels:[NSNumber numberWithInt:moveFactor]];
 	}	
 	
 	NSArray* arr = [dataDict objectForKey:@"Children"];
@@ -75,9 +81,19 @@
 	} else {
 		cell.accessoryType = UITableViewCellAccessoryNone;
 	}
+	cell.navController = self.navController;
 	return cell;
 }
-
+- (void) moveDownView: (UIView*) view byPixels: (NSNumber*) pixels {
+	CGRect frame = view.frame;
+	frame = CGRectMake(frame.origin.x, frame.origin.y + [pixels floatValue], frame.size.width, frame.size.height);
+	view.frame = frame;
+}
+- (void) clipButtonToItsTitleWidth: (UIButton*) btn {
+	UILabel* lbl = btn.titleLabel;
+	float width = [lbl.text sizeWithFont:lbl.font].width;
+	btn.frame = CGRectMake(btn.frame.origin.x, btn.frame.origin.y, width + 10, btn.frame.size.height);
+}
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 	NSDictionary* dataDict = [dataArray objectAtIndex: indexPath.row];
 	NSArray* arr = [dataDict objectForKey:@"Children"];
