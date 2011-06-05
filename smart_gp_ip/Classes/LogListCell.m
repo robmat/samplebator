c4ab86436b0d193574cf69eb6338e4c17ecec3d0
robbator <robbator@users.noreply.github.com>
2011-06-05 21:44:41 +0000

    Andys remarks.

--- smart_gp_ip/Classes/LogListCell.m
+++ smart_gp_ip/Classes/LogListCell.m
@@ -1,15 +1,43 @@
 #import "LogListCell.h"
 #import "LogScreenViewController.h"
+#import "SetReminderViewController.h"
 
 @implementation LogListCell
 
 @synthesize titleLbl, detailLbl, log, navController, tvc, locNot;
 
 - (IBAction) editAction: (id) sender {
-	LogScreenViewController* llvc = [[LogScreenViewController alloc] initWithNibName:nil bundle:nil];
-	llvc.log = log;
-	[self.navController pushViewController:llvc animated:YES];
-	[llvc release];
+	if (reminder) {
+		NSString* submitLabelStr = locNot.alertBody;
+		int index = 0;
+		SetReminderViewController* srvc = [[SetReminderViewController alloc] initWithNibName:nil bundle:nil];
+		if ([submitLabelStr rangeOfString:@"MEETING"].location != NSNotFound) {
+			index = [submitLabelStr rangeOfString:@"MEETING"].length;
+			srvc.title = @"Meeting reminder";
+		}
+		if ([submitLabelStr rangeOfString:@"PHONE CALL"].location != NSNotFound) {
+			index = [submitLabelStr rangeOfString:@"PHONE CALL"].length;
+			srvc.title = @"Phone call reminder";
+		}
+		if ([submitLabelStr rangeOfString:@"CHECK PATIENT"].location != NSNotFound) {
+			index = [submitLabelStr rangeOfString:@"CHECK PATIENT"].length;
+			srvc.title = @"Check patient reminder"; 
+		}
+		if ([submitLabelStr rangeOfString:@"CHECK RESULT"].location != NSNotFound) {
+			index = [submitLabelStr rangeOfString:@"CHECK RESULT"].length;
+			srvc.title = @"Check result reminder"; 
+		}
+		srvc.locNot = locNot;
+		srvc.submitLabelText = [submitLabelStr substringToIndex:index];
+		srvc.actionStr = [submitLabelStr substringFromIndex:index];
+		[self.navController pushViewController:srvc animated:YES];
+		[srvc release];
+	} else {
+		LogScreenViewController* llvc = [[LogScreenViewController alloc] initWithNibName:nil bundle:nil];
+		llvc.log = log;
+		[self.navController pushViewController:llvc animated:YES];
+		[llvc release];
+	}
 }
 - (IBAction) deleteAction: (id) sender {
 	UIAlertView* alert = [[UIAlertView alloc] initWithTitle:@"Really delete?" 
@@ -22,15 +50,19 @@
 }
 - (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
 	if (buttonIndex == 1) {
-		NSMutableArray* logs = [NSMutableArray arrayWithContentsOfFile:[LogScreenViewController getFilePath]];
-		for (NSDictionary* logDict in logs) {
-			NSNumber* time1 = [logDict objectForKey:@"Id"];
-			NSNumber* time2 = [log objectForKey:@"Id"];
-			if ([time1 isEqualToNumber:time2]) {
-				[logs removeObject:logDict];
+		if (reminder) {
+			[[UIApplication sharedApplication] cancelLocalNotification:locNot];
+		} else {
+			NSMutableArray* logs = [NSMutableArray arrayWithContentsOfFile:[LogScreenViewController getFilePath]];
+			for (NSDictionary* logDict in logs) {
+				NSNumber* time1 = [logDict objectForKey:@"Id"];
+				NSNumber* time2 = [log objectForKey:@"Id"];
+				if ([time1 isEqualToNumber:time2]) {
+					[logs removeObject:logDict];
+				}
 			}
+			[logs writeToFile:[LogScreenViewController getFilePath] atomically:YES];
 		}
-		[logs writeToFile:[LogScreenViewController getFilePath] atomically:YES];
 		[tvc.tableView reloadData];
 	}
 }
--- smart_gp_ip/Classes/SetReminderViewController.h
+++ smart_gp_ip/Classes/SetReminderViewController.h
@@ -7,12 +7,16 @@
 	NSString* submitLabelText;
 	IBOutlet UIDatePicker* datePicker;
 	IBOutlet UILabel* submitLabel;
+	UILocalNotification* locNot;
+	NSString* actionStr;
 }
 
 @property (nonatomic, retain) UITextView* textView;
 @property (nonatomic, retain) NSString* submitLabelText;
 @property (nonatomic, retain) UIDatePicker* datePicker;
 @property (nonatomic, retain) UILabel* submitLabel;
+@property (nonatomic, retain) UILocalNotification* locNot;
+@property (nonatomic, retain) NSString* actionStr;
 
 - (IBAction) setReminder;
 - (void) animateTextField: (UITextView*) textField up: (BOOL) up;
