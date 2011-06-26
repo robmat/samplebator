fb1f29d16bef0c838dfdde3b016231b27e8668b3
robbator <robbator@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-06-26 10:45:01 +0000

    Verband management end.

--- trunk/dartliga_php/LVP/admin_verband_management.php
+++ trunk/dartliga_php/LVP/admin_verband_management.php
@@ -15,7 +15,7 @@ function verband_list() {
 	$ret = $ret.'<td class="thead">Name</td><td class="thead">Verband logic</td><td class="thead">Edit</td></tr>';
 	
 	while ( list( $id, $vcode, $vname, $vlogic, $vactive ) = sql_fetch_row( $verband_result, $dbi ) ) {
-		$ret = $ret.'<tr><td>'.( $user_active ? 'Yes' : 'No').'</td>';
+		$ret = $ret.'<tr><td>'.( $vactive == 1 ? 'Yes' : 'No').'</td>';
 		$ret = $ret.'<td>'.$id.'</td><td>'.$vcode.'</td><td>'.$vname.'</td><td>'.$vlogic.'</td>';
 		$location_path = 'admin_verband_management.php?op=new_verband&vid='.$id;
 		$ret = $ret.'<td><img src="images/edit24.png" style="cursor: pointer;" onclick="window.location.href = \''.$location_path.'\'" /></td></tr>';
@@ -40,18 +40,59 @@ function verband_form() {
 		list( $id, $vcode, $vname, $vlogic, $vactive ) = sql_fetch_row( $verband_result, $dbi );
 		$vid = '&vid='.$vid;
 	}
-	$ret = '<h3>Verband und berecht</h3><table border="0" cellpadding="15" align="center"><tr>';
+	$ret = '<form action="admin_verband_management.php?op=new_verband_creation'.$vid.'" method="post">';
+	$ret = $ret.'<h3>Verband und berecht</h3><table border="0" cellpadding="0" align="left">';
 	$ret = $ret.'<tr><td>Verband name: </td><td>'._input( 1, 'vname', $vname, 50, 50 ).'</td></tr>';
 	$ret = $ret.'<tr><td>Verband code: </td><td>'._input( 1, 'vcode', $vcode, 50, 50 ).'</td></tr>';
 	$ret = $ret.'<tr><td>Verband logic:</td><td>'._input( 1, 'vlogic', $vlogic, 50, 50 ).'</td></tr>';
-	$ret = $ret.'<tr><td>Active:</td><td><select id="uactive" name="uactive">';
-	$ret = $ret.'<option value="1" '.( $uactive == '1' ? 'selected="selected"' : '' ).'>Active</option>';
-	$ret = $ret.'<option value="0" '.( $uactive == '0' ? 'selected="selected"' : '' ).'>Inactive</option>';
+	$ret = $ret.'<tr><td>Active:</td><td><select id="vactive" name="vactive">';
+	$ret = $ret.'<option value="1" '.( $vactive == 1 ? 'selected="selected"' : '' ).'>Active</option>';
+	$ret = $ret.'<option value="0" '.( $vactive == 0 ? 'selected="selected"' : '' ).'>Inactive</option>';
 	$ret = $ret.'</select></td></tr>';
-	$ret = $ret.'</table>';
+	$ret = $ret.'</table>'._button( !empty( $_REQUEST['vid'] ) ? 'Update verband' : 'Create verband' ).'</form>';
 	return $ret;
 }
+#Validate verband form
+function verband_validate_form() {
+	global $dbi;
 
+	$ret = '<div style="color: red; padding: 5px;">';
+	if ( empty( $_REQUEST['vname'] ) ) { $ret = $ret.'Name required!<br/>'; }
+	if ( empty( $_REQUEST['vcode'] ) ) { $ret = $ret.'Code required!<br/>'; }
+	$ret = $ret.'</div>';
+	
+	if ( strcmp( $ret, '<div style="color: red; padding: 5px;"></div>' )  == 0 ) { # valid
+		$vname = $_REQUEST['vname'];
+		$vcode = $_REQUEST['vcode'];
+		$vlogic = $_REQUEST['vlogic'];
+		$vactive = $_REQUEST['vactive'];
+		$vid = $_REQUEST['vid'];
+		
+		$sql = 'INSERT INTO tverband (id, vcode, vname, vlogic, version, vactive) VALUES ';
+		$sql = $sql.' (0, "'.$vcode.'", "'.$vname.'", "'.$vlogic.'", "1", '.$vactive.') ';
+		
+		if ( isset( $vid ) && !empty( $vid ) ) {
+			$sql = 'UPDATE tverband SET vname = "'.$vname.'", vcode = "'.$vcode.'", vlogic = "'.$vlogic.'", vactive = '.$vactive.' WHERE id = '.$vid;
+		}
+		
+		$insert_update_result = sql_query( $sql, $dbi );
+
+		if ($insert_update_result == TRUE) {
+			$ret = $ret.'<script> window.location.href = "admin_verband_management.php?op=new_verband_created" </script>';		
+		} else {
+			$ret = $ret.'<div style="color: red;">Creation/update of a berband failed for unknown reasons!</div>';		
+		}
+	} else {
+		$ret = $ret.verband_form();
+	}
+	return $ret;
+}
+# After succesful user creation show summary. 
+function verband_created() {
+	$ret = 'Creation/update successful!<br/><br/>';
+	$ret = $ret.verband_list();
+	return $ret;
+}
 # START OUTPUT
 
 LS_page_start('empty');
@@ -69,10 +110,9 @@ if ( !isset( $usertoken ) ) {
 
 	switch ( $myop ) {
 		case 'verband_list': { echo verband_list(); break; }
-		case 'new_user': { echo verband_form(); break;	}
-		case 'new_user_creation': { echo user_validate_form(); break; }
-		case 'new_user_created': { echo user_created(); break; }
-		case 'reset_user_failcount': { echo reset_user_failcount( $_REQUEST['user_id'] ); break; }
+		case 'new_verband': { echo verband_form(); break;	}
+		case 'new_verband_creation': { echo verband_validate_form(); break; }
+		case 'new_verband_created': { echo verband_created(); break; }
 	}
 	echo '</td></tr></table>'; # end table
 }
