67807b135b69caf1e33e7e3149ad4853c93ca9a9
robbator <robbator@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-06-26 09:10:49 +0000

    Meldewesen rechte.

--- trunk/dartliga_php/LVP/admin_main_menu.php
+++ trunk/dartliga_php/LVP/admin_main_menu.php
@@ -28,6 +28,10 @@ if ( !isset( $usertoken ) ) {
 	$ret = $ret._usermenuitem('System benutzer typen', 'admin_system_user_types.php', 'System benutzer typen', 'people.gif');
 	$ret = $ret._usermenuitem('Liga gruppe rechte zuweisung', 'admin_division_rights_to_users.php', 'Liga gruppe rechte zuweisung', 'optimize.gif');
 	$ret = $ret._usermenuitem('Liga gruppe rechte typen', 'admin_division_rights.php', 'Liga gruppe rechte typen', 'optimize.gif');
+	$ret = $ret.'</tr><tr>.';
+	$ret = $ret._usermenuitem('Bereiche landesvarbände', 'admin_system_user_types.php', 'System benutzer typen', 'people.gif');
+	$ret = $ret._usermenuitem('Meldewesen rechte', 'admin_verband_rights.php', 'Liga gruppe rechte zuweisung', 'optimize.gif');
+	$ret = $ret._usermenuitem('Meldewesen rechte zuweisung', 'admin_division_rights.php', 'Liga gruppe rechte typen', 'optimize.gif');
 	# $ret = $ret.'</tr></table>'
 	echo $ret;
 }
