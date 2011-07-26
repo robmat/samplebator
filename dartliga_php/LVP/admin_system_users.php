be6a3489cb662b71ab4d088dcc10fbe012942b33
robbator <robbator@users.noreply.github.com>
2011-07-26 18:08:22 +0000

    User authorization.

--- dartliga_php/LVP/admin_system_users.php
+++ dartliga_php/LVP/admin_system_users.php
@@ -188,7 +188,9 @@ LS_page_start('empty');
 
 if ( isset( $_REQUEST['op'] ) && strlen( $_REQUEST['op'] ) < 25 ) { $myop=strip_tags( $_REQUEST['op'] ); } else { $myop = "user_list"; }
 
-if ( !isset( $usertoken ) ) {
+$user_access_level = (int) $usertoken["usertype_id"];
+
+if ( !isset( $user_access_level ) && $user_access_level > 5 ) { //sys admin only
 	echo '<script> window.location.href = "/dso_user.php" </script>';
 } else {
 	echo '<h3>System benutzer</h3><table class="box"><tr><td>'; # header and start table
