d82e587da1313fa3fa64e24851070ee7ba37c043
robbator <robbator@users.noreply.github.com>
2011-07-27 20:14:02 +0000

    ifonly start

---
+++ if_only_ip/Classes/if_only_ipAppDelegate.h
@@ -0,0 +1,10 @@
+#import <UIKit/UIKit.h>
+
+@interface if_only_ipAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
