5c8d2faa4d22c6bd6c032cb39a8300b1acb2b2fa
robbator <robbator@users.noreply.github.com>
2011-08-07 15:44:18 +0000

    YT upload ready. Prawie.

--- ifonly_ip/Classes/UploadVideoMenuVC.h
+++ ifonly_ip/Classes/UploadVideoMenuVC.h
@@ -1,15 +1,20 @@
 #import <UIKit/UIKit.h>
 #import "AVPlayerView.h"
+#import "GData.h"
 
 @interface UploadVideoMenuVC : UIViewController {
 	AVPlayer *avplayer;
 	IBOutlet AVPlayerView *playerView;
 	UITextField* tagsText;
 	UITextField* noteText;
+	GDataServiceGoogleYouTube* ytService;
+	NSString* educationCategory;
 }
 
 @property (nonatomic, retain) AVPlayer *avplayer;
 @property (nonatomic, retain) IBOutlet AVPlayerView *playerView;
+@property (nonatomic, retain) GDataServiceGoogleYouTube* ytService;
+@property (nonatomic, retain) NSString* educationCategory;
 
 - (IBAction) aboutAction: (id) sender;
 - (IBAction) addNoteAction: (id) sender;
