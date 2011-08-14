4f867f29447fd585936252c5fa307e1c5170888c
robbator <robbator@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-08-14 15:12:19 +0000

    Video record.

--- trunk/ifonly_and/src/com/bator/ifonly/SplashActivity.java
+++ trunk/ifonly_and/src/com/bator/ifonly/SplashActivity.java
@@ -1,13 +1,23 @@
 package com.bator.ifonly;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SplashActivity extends Activity {
-    /** Called when the activity is first created. */
+    private ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(1);
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.splash);
+        scheduler.schedule(new Runnable() {
+			@Override
+			public void run() {
+				startActivity(new Intent("ifonly.mainmenu"));
+			}
+		}, 1, TimeUnit.SECONDS);
     }
 }
