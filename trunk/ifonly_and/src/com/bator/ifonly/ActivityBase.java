94c81c4c1c439d37548cccdeb4b7e9c62c34a196
robbator <robbator@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2011-08-21 12:58:33 +0000

    Some adjustments. Error reporting and plak playing.

--- trunk/ifonly_and/src/com/bator/ifonly/ActivityBase.java
+++ trunk/ifonly_and/src/com/bator/ifonly/ActivityBase.java
@@ -1,12 +1,14 @@
 package com.bator.ifonly;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.TextView;
 
 public class ActivityBase extends Activity {
+	private MediaPlayer mp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -18,6 +20,7 @@ public class ActivityBase extends Activity {
 			backBtn.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
+					playPlak();
 					ActivityBase.this.finish();
 				}
 			});
@@ -28,5 +31,16 @@ public class ActivityBase extends Activity {
 		if (titleView != null) {
 			titleView.setText(title);
 		}
-	} 
+	}
+	public void playPlak() {
+		if (mp == null) {
+			mp = MediaPlayer.create(this, R.raw.plak);
+		}
+		mp.start();
+	}
+	@Override
+	protected void onDestroy() {
+		mp.release();
+		super.onDestroy();
+	}
 }
