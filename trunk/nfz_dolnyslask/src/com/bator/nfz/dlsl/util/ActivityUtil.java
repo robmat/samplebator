ba9127c94b5fcb9e32ad8fe7907c15458fd0ce0f
robbator@gmail.com <robbator@gmail.com@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2012-03-24 10:25:55 +0000

    NFZ dolny slask first screen.

---
+++ trunk/nfz_dolnyslask/src/com/bator/nfz/dlsl/util/ActivityUtil.java
@@ -0,0 +1,38 @@
+package com.bator.nfz.dlsl.util;
+
+import com.bator.nfz.dlsl.R;
+import com.bator.nfz.dlsl.app.NfzApp;
+
+import android.app.Activity;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.net.ConnectivityManager;
+
+public class ActivityUtil {
+	public static boolean isOnline(Activity activity) {
+	    ConnectivityManager cm = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+	    return cm.getActiveNetworkInfo().isConnectedOrConnecting();
+	}
+	public static Dialog showDialog(Activity activity, int title, int msg) {
+		Builder builder = new Builder(activity).setTitle(title);
+		builder.setMessage(msg).setPositiveButton("Ok", new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		return builder.create();
+	}
+	public static Dialog showErrDialog(Activity activity, Exception e) {
+		String errMsg = NfzApp.nfzApp.getString(R.string.err_msg) + " " + e.getClass() + ": " + e.getMessage();
+		Builder builder = new Builder(activity).setTitle(R.string.err);
+		builder.setMessage(errMsg).setPositiveButton("Ok", new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		return builder.create();
+	}
+}
