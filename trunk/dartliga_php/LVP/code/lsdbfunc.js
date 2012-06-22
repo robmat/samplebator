f641507b3066030bb04731a76bda500740de1109
robbator <robbator@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2012-06-22 14:17:17 +0000

    LVP: Depacked.

--- trunk/dartliga_php/LVP/code/lsdbfunc.js
+++ trunk/dartliga_php/LVP/code/lsdbfunc.js
@@ -1,1 +1,358 @@
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(c/a))+String.fromCharCode(c%a+161)};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'[\xa1-\xff]+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp(e(c),'g'),k[c])}}return p}('$(»).£Ü(¡(){$(".¢¿").¢À(¡(){¢¶(æ)},¡(){¢¸(æ)})});¡ ¢¶(§){§.¯.¿="Ñ";§.¯.Ì="#£Ö";§.¯.Æ="¢³"};¡ ¢¸(§){§.¯.¿="£Ô";§.¯.Ì="#£Ó";§.¯.Æ="Å"};¡ ¢Ä(§){§.¯.¿="#¢Å";§.¯.Ì="#¢Æ";§.¯.Æ="¢³"};¡ ¢É(§){§.¯.¿="#¢Ê";§.¯.Ì="Ñ";§.¯.Æ="Å"};¡ ¢Ì(ß){« c=ß.¢Í.¢·;¢µ(i=0;i<c;i++){ß.£Ç(0)}};¡ ¢Ð(Ò,¢¬){».Ó(Ò).é=\'\';».Ó(Ò).é=¢¬};¡ ¢Ò(){« °=$(\'#­\').¦();$(\'#¢Ó\').¥(\'<µ ¯=\\\'Ü:£¿\\\'>&£¾;</µ><ì ¸=\\\'ð\\\'></ì><µ ¸=È><µ ¸=í></µ><µ ¸=¢½></µ></µ>\');$(\'#£»\').¥(\'\');$.©(\'²/¢×.£\',{­:°},¡(¢){$(\'#í\').¥(¢)});$.©(\'²/¢Ø.£\',{­:°,¢°:¢±},¡(¢){$(\'#¢½\').¥(¢)})};¡ £¶(¢©){« ¢¥=¢©.¢­;« y=À ¢Û;« s=\'\';« e=\'\';« ¹=y.¢Ü();Ú(¢¥){®\'2\':Ë(y.¢Þ()<6){s=(¹-1)+\'-ñ-Ê\';e=¹+\'-¢¤-×\'}¢«{s=¹+\'-ñ-Ê\';e=(¹+1)+\'-¢¤-×\'}¬;Å:s=¹+\'-Ê-Ê\';e=¹+\'-¢á-×\';¬}$(\'#â\').¦(s);$(\'#á\').¦(e)};¡ £ª(){Ö(\'¶\');$.©(\'·/¢ã.£\',{´:$(\'#´\').¦(),¢ä:$(\'#£§\').¦(),¶:$(\'#¶\').¦()},¡(¢){$(\'#Ï\').¦(¢)})};¡ £¦(v){« ù=$(\'#£¥\').¦();$(\'#ü\').¥(\'¢ç ...\');$.©(\'²/¢é.£\',{¢ê:v,¢ì:ù,ø:ý(\'ú\')},¡(¢){$(\'#ü\').¥(¢)})};¡ ¢ý(){$(\'#È\').¥(\'<p>¢ü ¢ï ...</p>\');« r=$(\'#¢ú\').¦();« t=$(\'#¢ù\').¦();« c=$(\'#¢ò\').¢ª(\'¢ó\');$.©(\'·/½.£\',{¾:\'É\',¢ô:r,¶:t,Ý:c},¡(¢){$(\'#È\').¥(¢)})};¡ ¢õ(t,ø){« v=$(\'#´\').¦();$.©(\'·/½.£\',{¾:\'É\',´:v,¶:t,Ý:\'¢ö\'},¡(¢){$(\'#¢÷\').¥(¢)})};¡ Î(¤){$.©(\'·/½.£\',{¤:¤,¾:\'¢ø\'},¡(¢){$(\'#¢þ\').¥(¢)})};¡ ú(ò,õ){ª.¨=\'¢¦.£?±=¢ÿ&­=\'+ý(õ)+\'&£¡=&¤=\'+ò.¸};¡ £¢(Õ){££=ª.¢®(\'²/£¤.£?Õ=\'+Õ,"£«","ç=¼,¢º=¼,å=¼,¨=¼,ë=£­,Ü=£®,¢¡=¢§,¢£=¢§")};¡ £±(){{$.©(\'²/£³.£\',{£´:$(\'#£·\').¦()},¡(¢){$("#£¸").¥(¢)})}};¡ £º(¤){$.©(\'²/£¼.£\',{¤:¤,£Á:1,£Â:$(\'#¸\').¦()},¡(¢){$("#Ð").¥(¢)})};¡ £Ã(¤){$.©(\'²/£Ä.£\',{¤:¤,´:0,Ý:0},¡(¢){$("#Ð").¥(¢)})};¡ £Å(¤,¢¯){$.©(\'²/£È.£\',{¤:¤,£É:¢¯,¢°:¢±},¡(¢){$("#Ð").¥(¢)})};¡ £Ë(³,¤){Ç(\'£Ì £Î:\'+³+\' ¢µ £Ð \'+¤);$.©(\'·/½.£\',{¤:¤,³:³,¾:\'£Ñ\'},¡(¢){$("#è").¥(¢);Î(¤)})};¡ £Õ(³,¤){Ö(\'¶\');$.©(\'·/½.£\',{¤:¤,³:³,´:$("#´").¦(),Ï:$("#Ï").¦(),á:$("#á").¦(),â:$("#â").¦(),¶:$("#¶").¦(),¾:\'£Ù\'},¡(¢){$("#£Ú").¥(¢);Î(¤)})};¡ £Û(³,¤){$.©(\'·/½.£\',{¤:¤,³:³,¾:\'Ù\'},¡(¢){$("#è").¥(¢)})};¡ ¢Á(¤){ª.¨=\'Û.£?±=Ù&¢Â=\'+¤};¡ ¢Ç(´){ª.¨=\'Ô.£?±=Ù&¢Ë=\'+´};¡ ¢Î(Á){« r=$(\'#Â\').¦();Ú(Á){® 1:ª.¨=\'Û.£?±=É&Â=\'+r;¬;® 2:ª.¨=\'Ô.£?±=É&Â=\'+r;¬;® 3:ª.¨=\'Û.£?±=À&Â=\'+r;¬;® 4:ª.¨=\'Ô.£?±=À&Â=\'+r;¬}};¡ ¢Õ(Á){« °=$(\'#­\').¦();Ú(Á){® 1:ª.¨=\'÷.£?±=¢Ù&­=\'+°;¬;® 2:ª.¨=\'î.£?±=¢Ú&­=\'+°;¬;® 3:ª.¨=\'î.£?±=¢Ý&­=\'+°;¬;® 4:ª.¨=\'¢¦.£?­=\'+°;¬;® 5:ª.¨=\'¢à.£?­=\'+°;¬;® 6:$(\'#ð\').¥(\'¢â ¢å\\¢æ\');$.©(\'²/¢è.£\',{­:°,¢ë:\'ä\'},¡(¢){$(\'#È\').¥(¢)});¬;® 7:ª.¨=\'¢î.£?­=\'+°;¬;® 8:ª.¨=\'¢ð.£?±=¢ñ&­=\'+°;¬;® 9:ª.¨=\'÷.£?±=¢û&­=\'+°;¬;Å:Ç(Á+\' £¨ £¬ a £¯ £²\');¬}};¡ £¹(¢´){« Í=».£½(\'Í\').§(0);« Þ=».Ó(\'¢¼\');Ë(Þ)Í.£Æ(Þ);º=».£Ê(\'º\');º.£Í=¢´;º.£Ò=\'£×/£Ø\';º.¸=\'¢¼\';Í.¢¾(º)};¡ ¢Ã(Ã){Ø{$(\'#¢¹\').¥(\'\');$.©(\'²/¢È.£\',{¢¢:Ã},¡(¢){$("#¢¹").¥(¢)})}ã(e){Ç(\'û þ ¢Ï::¢Ñ ¢Ô ...\')}};¡ ¢Ö(Ã){Ø{$(\'#ô\').¥(\'\');$.©(\'²/¢ß.£\',{¢¢:Ã},¡(¢){$("#ô").¥(¢)})}ã(e){Ç(\'û þ £© £°\')}};¡ £µ(¢²){£À=ª.¢®(¢²,"£Ï","ç=ä,¢º=ä,å=¼,¨=¼,ë=ï,Ü=ï,¢¡=0,¢£=0")};¡ Ö(Ä){« à=$(\'#\'+Ä).¢ª(\'¢­\');Ø{Ë(à.¢·<1){ê À ÿ(\'ö\')}Ë(à==0){ê À ÿ(\'ö\')}¢«{$(\'#\'+Ä).ó(\'¢¨-¿\',\'Ñ\');¢» 0}}ã(e){$(\'#\'+Ä).ó(\'¢¨-¿\',\'#¢í\');¢» 1}};',95,250,'function|data|php|pid|html|val|item|location|post|window|var|break|eventid|case|style|ev|func|snippets|mid|vid|div|mtype|lsdb|id|sy|script|document|no|Membership|action|color|new|btn|realmid|teamID|objID|default|cursor|alert|maincontent|list|01|if|backgroundColor|head|listmemberp|vpassnr|check|white|element|getElementById|dso_verein|locid|checkempty|31|try|edit|switch|dso_player|height|mactive|scriptTag|tbl|chkval|vmend|vmstart|catch|yes|menubar|this|resizable|frmMember|innerHTML|throw|width|h3|mg|ls_system|400|pagetitle|08|oBtn|css|lineUpPerf|evid|LSDB|lsdbTeam|paction|sc|rankingdetails|Cannot|qry|escape|show|Error|left|teamid|top|07|ty|ls_stats|50|background|obj|attr|else|sMsg|value|open|sid|limit|10|thisurl|pointer|file|for|mover|length|mout|lineUp|scrollbars|return|loadScript|mt|appendChild|lsdbbutton|hover|playeredit|vpid|getlineup|datarowover|000000|ccffcc|vereinedit|retLineUpTable|datarowout|444444|vvid|clearTable|rows|regbtnclick|LineUp|writeHTML|unsupported|selLigaChange|mainpane|browser|lsdbbtnclick|getlineupperf|retEventPerfPerRound|listEventTopWinners|browse|newmatch|Date|getFullYear|schedule|getMonth|retLineUpPerfTable|ls_tabelle|12|Alle|AutoPass|gdr|Spielst|xe4tten|fetching|retLocationTable|retEventTeamPlayer|vereinid|extend|eventstat|ffbbbb|ls_debug|daten|lsdbSys|teamperf|mcurrent|checked|mrealm|listmemberv|true|memberv|listp|mtyper|vrealm|newteam|hole|listmemberr|lstMember|statlistdetail|vindexdate|showmap|FL|retLocationMap|statcode|vereinstatcodeplayer|vgender|is|LineUpTeam|genautopass|DartsMap|not|500|350|valid|Performance|historymatch|choice|showMatchChangeHistory|matchkey|OpenSysWin|setmdate|vmkey|hmatch|loadContent|chkplayerteam|foot|retLineUpPlayer|getElementsByTagName|nbsp|5px|F1|lactive|wfid|chkplayermember|listMembership|chkplayerstat|removeChild|deleteRow|retStaticStatVal|statid|createElement|memberdel|Deleting|src|Member|SysWin|Player|delete|type|f8f8f8|black|membersave|202088|text|javascript|save|savemsg|memberedit|ready'.split('|'),0,{}))
+$(document).ready(function () {
+    $(".lsdbbutton").hover(function () {
+        mover(this)
+    }, function () {
+        mout(this)
+    })
+});
+
+function mover(item) {
+    item.style.color = "white";
+    item.style.backgroundColor = "#202088";
+    item.style.cursor = "pointer"
+};
+
+function mout(item) {
+    item.style.color = "black";
+    item.style.backgroundColor = "#f8f8f8";
+    item.style.cursor = "default"
+};
+
+function datarowover(item) {
+    item.style.color = "#000000";
+    item.style.backgroundColor = "#ccffcc";
+    item.style.cursor = "pointer"
+};
+
+function datarowout(item) {
+    item.style.color = "#444444";
+    item.style.backgroundColor = "white";
+    item.style.cursor = "default"
+};
+
+function clearTable(tbl) {
+    var c = tbl.rows.length;
+    for (i = 0; i < c; i++) {
+        tbl.deleteRow(0)
+    }
+};
+
+function writeHTML(element, sMsg) {
+    document.getElementById(element).innerHTML = '';
+    document.getElementById(element).innerHTML = sMsg
+};
+
+function selLigaChange() {
+    var ev = $('#eventid').val();
+    $('#mainpane').html('<div style=\'height:5px\'>&nbsp;</div><h3 id=\'pagetitle\'></h3><div id=maincontent><div id=mg></div><div id=mt></div></div>');
+    $('#foot').html('');
+    $.post('snippets/retEventPerfPerRound.php', {
+        eventid: ev
+    }, function (data) {
+        $('#mg').html(data)
+    });
+    $.post('snippets/listEventTopWinners.php', {
+        eventid: ev,
+        limit: 10
+    }, function (data) {
+        $('#mt').html(data)
+    })
+};
+
+function setmdate(obj) {
+    var ty = obj.value;
+    var y = new Date;
+    var s = '';
+    var e = '';
+    var sy = y.getFullYear();
+    switch (ty) {
+    case '2':
+        if (y.getMonth() < 6) {
+            s = (sy - 1) + '-08-01';
+            e = sy + '-07-31'
+        } else {
+            s = sy + '-08-01';
+            e = (sy + 1) + '-07-31'
+        }
+        break;
+    default:
+        s = sy + '-01-01';
+        e = sy + '-12-31';
+        break
+    }
+    $('#vmstart').val(s);
+    $('#vmend').val(e)
+};
+
+function genautopass() {
+    checkempty('mtype');
+    $.post('lsdb/AutoPass.php', {
+        vid: $('#vid').val(),
+        gdr: $('#vgender').val(),
+        mtype: $('#mtype').val()
+    }, function (data) {
+        $('#vpassnr').val(data)
+    })
+};
+
+function vereinstatcodeplayer(v) {
+    var sc = $('#statcode').val();
+    $('#qry').html('fetching ...');
+    $.post('snippets/retEventTeamPlayer.php', {
+        vereinid: v,
+        eventstat: sc,
+        paction: escape('rankingdetails')
+    }, function (data) {
+        $('#qry').html(data)
+    })
+};
+
+function listmemberr() {
+    $('#maincontent').html('<p>hole daten ...</p>');
+    var r = $('#vrealm').val();
+    var t = $('#mtyper').val();
+    var c = $('#mcurrent').attr('checked');
+    $.post('lsdb/Membership.php', {
+        action: 'list',
+        mrealm: r,
+        mtype: t,
+        mactive: c
+    }, function (data) {
+        $('#maincontent').html(data)
+    })
+};
+
+function listmemberv(t, paction) {
+    var v = $('#vid').val();
+    $.post('lsdb/Membership.php', {
+        action: 'list',
+        vid: v,
+        mtype: t,
+        mactive: 'true'
+    }, function (data) {
+        $('#memberv').html(data)
+    })
+};
+
+function listmemberp(pid) {
+    $.post('lsdb/Membership.php', {
+        pid: pid,
+        action: 'listp'
+    }, function (data) {
+        $('#lstMember').html(data)
+    })
+};
+
+function rankingdetails(oBtn, evid) {
+    window.location = 'ls_stats.php?func=statlistdetail&eventid=' + escape(evid) + '&vindexdate=&pid=' + oBtn.id
+};
+
+function showmap(locid) {
+    FL = window.open('snippets/retLocationMap.php?locid=' + locid, "DartsMap", "resizable=no,scrollbars=no,menubar=no,location=no,width=500,height=350,left=50,top=50")
+};
+
+function historymatch() {
+    {
+        $.post('snippets/showMatchChangeHistory.php', {
+            matchkey: $('#vmkey').val()
+        }, function (data) {
+            $("#hmatch").html(data)
+        })
+    }
+};
+
+function chkplayerteam(pid) {
+    $.post('snippets/retLineUpPlayer.php', {
+        pid: pid,
+        lactive: 1,
+        wfid: $('#id').val()
+    }, function (data) {
+        $("#check").html(data)
+    })
+};
+
+function chkplayermember(pid) {
+    $.post('snippets/listMembership.php', {
+        pid: pid,
+        vid: 0,
+        mactive: 0
+    }, function (data) {
+        $("#check").html(data)
+    })
+};
+
+function chkplayerstat(pid, sid) {
+    $.post('snippets/retStaticStatVal.php', {
+        pid: pid,
+        statid: sid,
+        limit: 10
+    }, function (data) {
+        $("#check").html(data)
+    })
+};
+
+function memberdel(mid, pid) {
+    alert('Deleting Member:' + mid + ' for Player ' + pid);
+    $.post('lsdb/Membership.php', {
+        pid: pid,
+        mid: mid,
+        action: 'delete'
+    }, function (data) {
+        $("#frmMember").html(data);
+        listmemberp(pid)
+    })
+};
+
+function membersave(mid, pid) {
+    checkempty('mtype');
+    $.post('lsdb/Membership.php', {
+        pid: pid,
+        mid: mid,
+        vid: $("#vid").val(),
+        vpassnr: $("#vpassnr").val(),
+        vmend: $("#vmend").val(),
+        vmstart: $("#vmstart").val(),
+        mtype: $("#mtype").val(),
+        action: 'save'
+    }, function (data) {
+        $("#savemsg").html(data);
+        listmemberp(pid)
+    })
+};
+
+function memberedit(mid, pid) {
+    $.post('lsdb/Membership.php', {
+        pid: pid,
+        mid: mid,
+        action: 'edit'
+    }, function (data) {
+        $("#frmMember").html(data)
+    })
+};
+
+function playeredit(pid) {
+    window.location = 'dso_player.php?func=edit&vpid=' + pid
+};
+
+function vereinedit(vid) {
+    window.location = 'dso_verein.php?func=edit&vvid=' + vid
+};
+
+function regbtnclick(btn) {
+    var r = $('#realmid').val();
+    switch (btn) {
+    case 1:
+        window.location = 'dso_player.php?func=list&realmid=' + r;
+        break;
+    case 2:
+        window.location = 'dso_verein.php?func=list&realmid=' + r;
+        break;
+    case 3:
+        window.location = 'dso_player.php?func=new&realmid=' + r;
+        break;
+    case 4:
+        window.location = 'dso_verein.php?func=new&realmid=' + r;
+        break
+    }
+};
+
+function lsdbbtnclick(btn) {
+    var ev = $('#eventid').val();
+    switch (btn) {
+    case 1:
+        window.location = 'lsdbTeam.php?func=browse&eventid=' + ev;
+        break;
+    case 2:
+        window.location = 'ls_system.php?func=newmatch&eventid=' + ev;
+        break;
+    case 3:
+        window.location = 'ls_system.php?func=schedule&eventid=' + ev;
+        break;
+    case 4:
+        window.location = 'ls_stats.php?eventid=' + ev;
+        break;
+    case 5:
+        window.location = 'ls_tabelle.php?eventid=' + ev;
+        break;
+    case 6:
+        $('#pagetitle').html('Alle Spielst\xe4tten');
+        $.post('snippets/retLocationTable.php', {
+            eventid: ev,
+            extend: 'yes'
+        }, function (data) {
+            $('#maincontent').html(data)
+        });
+        break;
+    case 7:
+        window.location = 'ls_debug.php?eventid=' + ev;
+        break;
+    case 8:
+        window.location = 'lsdbSys.php?func=teamperf&eventid=' + ev;
+        break;
+    case 9:
+        window.location = 'lsdbTeam.php?func=newteam&eventid=' + ev;
+        break;
+    default:
+        alert(btn + ' is not a valid choice');
+        break
+    }
+};
+
+function loadContent(file) {
+    var head = document.getElementsByTagName('head').item(0);
+    var scriptTag = document.getElementById('loadScript');
+    if (scriptTag) head.removeChild(scriptTag);
+    script = document.createElement('script');
+    script.src = file;
+    script.type = 'text/javascript';
+    script.id = 'loadScript';
+    head.appendChild(script)
+};
+
+function getlineup(teamID) {
+    try {
+        $('#lineUp').html('');
+        $.post('snippets/retLineUpTable.php', {
+            teamid: teamID
+        }, function (data) {
+            $("#lineUp").html(data)
+        })
+    } catch (e) {
+        alert('Cannot show LineUp::unsupported browser ...')
+    }
+};
+
+function getlineupperf(teamID) {
+    try {
+        $('#lineUpPerf').html('');
+        $.post('snippets/retLineUpPerfTable.php', {
+            teamid: teamID
+        }, function (data) {
+            $("#lineUpPerf").html(data)
+        })
+    } catch (e) {
+        alert('Cannot show LineUpTeam Performance')
+    }
+};
+
+function OpenSysWin(thisurl) {
+    F1 = window.open(thisurl, "SysWin", "resizable=yes,scrollbars=yes,menubar=no,location=no,width=400,height=400,left=0,top=0")
+};
+
+function checkempty(objID) {
+    var chkval = $('#' + objID).attr('value');
+    try {
+        if (chkval.length < 1) {
+            throw new Error('LSDB')
+        }
+        if (chkval == 0) {
+            throw new Error('LSDB')
+        } else {
+            $('#' + objID).css('background-color', 'white');
+            return 0
+        }
+    } catch (e) {
+        $('#' + objID).css('background-color', '#ffbbbb');
+        return 1
+    }
+};
