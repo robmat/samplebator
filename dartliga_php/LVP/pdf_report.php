dbccc3d6418d1a6d473eaccfb722fd19fa4d70ef
robbator <robbator@users.noreply.github.com>
2012-07-08 11:20:58 +0000

    LVP: PDF report fixed viewing when zeroes in short legs.

--- dartliga_php/LVP/pdf_report.php
+++ dartliga_php/LVP/pdf_report.php
@@ -7,106 +7,89 @@ require("api_rs.php");	// Backend DB API
 require("api_format.php");	// Backend DB API
 require('fpdf/fpdf.php');
 
-class PDF extends FPDF
-{
-	// Load data
-	function LoadData($file) {
-		// Read file lines
-		$lines = file($file);
-		$data = array();
-		foreach($lines as $line)
-		$data[] = explode(';',trim($line));
-		return $data;
-	}
-
-	// Simple table
-	function BasicTable($header, $data, $widths) {
-		// Header
-		foreach($header as $index => $col)
-		$this->Cell($widths[$index], 7, $col, 1);
+class PDF extends FPDF {
+	
+	public $widths = array(30, 40, 20, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 10, 10);
+	
+	function Header() {
+		$this->SetFont('Arial','B',9);
+		
+		$header = array('Name', 'Surname', 'HDSV nr.', '18', '17', '16', '15', '14', '13', '12', '11', '10', '9', '180er', '171er');
+		
+		$this->MultiCell(200, 2, "Sportabzeichen H.D.S.V. e.V. - Kategorie Short Leg, 180 und 171", 0, 'C', FALSE);		
 		$this->Ln();
+		$this->MultiCell(200, 2, "501-Wertungen: 18 Darts abwärts", 0, 'C', FALSE);
+		$this->Ln();
+		$this->MultiCell(200, 2, date('d.m.o G:i:s'), 0, 'C', FALSE);
+		
+		$this->Ln();
+		$this->Ln();
+		foreach($header as $index => $col) {
+			$this->Cell($this->widths[$index], 7, $col, 1, 0, 'C');
+		}
+		$this->Ln();
+		$this->SetFont('Arial','',9);
+	}
+	
+	function BasicTable($data) {
 		// Data
 		foreach($data as $row) {
-			foreach($row as $index =>  $col) {
-				$this->Cell($widths[$index], 6, $col, 1);
+			foreach($row as $index => $col) {
+				$this->SetFillColorBasedOnCell( $index, $col );
+				$this->Cell( $this->widths[$index], 6, $col, 1, 0, 'C', TRUE );
+				$this->SetFillColor( 255, 255, 255 );
 			}
 			$this->Ln();
 		}
 	}
-
-	// Better table
-	function ImprovedTable($header, $data) {
-		// Column widths
-		$w = array(40, 35, 40, 45);
-		// Header
-		for($i=0;$i<count($header);$i++)
-		$this->Cell($w[$i],7,$header[$i],1,0,'C');
-		$this->Ln();
-		// Data
-		foreach($data as $row)
-		{
-			$this->Cell($w[0],6,$row[0],'LR');
-			$this->Cell($w[1],6,$row[1],'LR');
-			$this->Cell($w[2],6,number_format($row[2]),'LR',0,'R');
-			$this->Cell($w[3],6,number_format($row[3]),'LR',0,'R');
-			$this->Ln();
+	
+	function SetFillColorBasedOnCell($columnIndex, $cellValue) {
+		//echo $columnIndex." ".$cellValue."<BR>";
+		if ($columnIndex == 13) {
+			if ($cellValue >= 30) {
+				$this->SetFillColor(255, 255, 0);
+			}
+			if ($cellValue < 29) {
+				$this->SetFillColor(192, 192, 192);
+			}
+			if ($cellValue < 14) {
+				$this->SetFillColor(204, 127, 50);
+			}
+			if ($cellValue < 5) {
+				$this->SetFillColor(255, 255, 255);
+			}
 		}
-		// Closing line
-		$this->Cell(array_sum($w),0,'','T');
-	}
-
-	// Colored table
-	function FancyTable($header, $data) {
-		// Colors, line width and bold font
-		$this->SetFillColor(255,0,0);
-		$this->SetTextColor(255);
-		$this->SetDrawColor(128,0,0);
-		$this->SetLineWidth(.3);
-		$this->SetFont('','B');
-		// Header
-		$w = array(40, 35, 40, 45);
-		for($i=0;$i<count($header);$i++)
-		$this->Cell($w[$i],7,$header[$i],1,0,'C',true);
-		$this->Ln();
-		// Color and font restoration
-		$this->SetFillColor(224,235,255);
-		$this->SetTextColor(0);
-		$this->SetFont('');
-		// Data
-		$fill = false;
-		foreach($data as $row)
-		{
-			$this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
-			$this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
-			$this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
-			$this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
-			$this->Ln();
-			$fill = !$fill;
-		}
-		// Closing line
-		$this->Cell(array_sum($w),0,'','T');
 	}
 }
 
 global $dbi;
 
-$queryResult = sql_query( "SELECT pl.pid AS playerid, pl.pfname AS name, pl.plname AS surname, pl.pfkey2 AS userkey, le.lstart, le.ldarts AS darts, le.lscore, le.lfinish, le.lhighscore, le.lhighscore171, COUNT(*) AS howmany, SUM(le.lhighscore) AS max180, SUM(le.lhighscore171) AS max171 FROM tplayer pl JOIN tblgameplayer gp ON gp.gppid = pl.pid JOIN tblgame ga ON ga.gid = gp.gpgid JOIN tblleg le ON le.lgid = ga.gid WHERE le.lscore = 501 GROUP BY playerid, le.ldarts ORDER BY surname, playerid, darts", $dbi );
+$queryResult = sql_query( "SELECT pl.pid AS playerid, pl.pfname AS name, pl.plname AS surname, pl.pfkey2 AS userkey, le.lstart, le.ldarts AS darts, le.lscore, le.lfinish, le.lhighscore, le.lhighscore171, COUNT(*) AS howmany, SUM(le.lhighscore) AS max180, SUM(le.lhighscore171) AS max171 FROM tplayer pl JOIN tblgameplayer gp ON gp.gppid = pl.pid JOIN tblgame ga ON ga.gid = gp.gpgid JOIN tblleg le ON le.lgid = ga.gid WHERE le.lscore = 501 AND pl.pfkey2 LIKE 'HE%' AND le.ldarts BETWEEN 9 AND 18 GROUP BY playerid, le.ldarts ORDER BY surname, playerid, darts", $dbi );
 $resultArray = createRecordSet( $queryResult, $dbi );
+
+$maxesQueryResult = sql_query( "SELECT playerid, name, surname, userkey, SUM(max180), SUM(max171) FROM (SELECT pl.pid AS playerid, pl.pfname AS name, pl.plname AS surname, pl.pfkey2 AS userkey, le.lstart, le.ldarts AS darts, le.lscore, le.lfinish, le.lhighscore, le.lhighscore171, COUNT(*) AS howmany, SUM(le.lhighscore) AS max180, SUM(le.lhighscore171) AS max171 FROM tplayer pl JOIN tblgameplayer gp ON gp.gppid = pl.pid JOIN tblgame ga ON ga.gid = gp.gpgid JOIN tblleg le ON le.lgid = ga.gid WHERE pl.pfkey2 LIKE 'HE%' GROUP BY playerid, le.ldarts ORDER BY surname, playerid, darts ) AS sub GROUP BY playerid", $dbi );
+$maxesResultArray = createRecordSet( $maxesQueryResult, $dbi );
 
 $currentPlayerId = "";
 $playerCount = 0;
 $pdfDataTable = array();
-$sum180max = 0;
-$sum171max = 0;
+$idsArray = array();
+
+
+foreach ( $resultArray as $index => $row ) {
+	$idsArray[] = $row[0];
+}
+foreach ( $maxesResultArray as $maxesRow ) {
+	$idsArray[] = $maxesRow[0];
+}
+
+$idsArray = array_unique( $idsArray );
 
 foreach ( $resultArray as $index => $row ) {
 	if ( $currentPlayerId != $row[0] ) {
 		$currentPlayerId = $row[0];
 		$pdfDataTable[$playerCount] = array();
 		$playerCount++;
-
-		$sum180max = 0;
-		$sum171max = 0;
 	}
 	$pdfDataTable[$playerCount - 1][0] = $row[1];
 	$pdfDataTable[$playerCount - 1][1] = $row[2];
@@ -122,30 +105,40 @@ foreach ( $resultArray as $index => $row ) {
 		}
 		$dartCounter++;
 	}
-
-	$sum180max += $row[11];
-	$sum171max += $row[12];
 	
-	$pdfDataTable[$playerCount - 1][13] = $sum180max;
-	$pdfDataTable[$playerCount - 1][14] = $sum171max;
+	foreach ( $maxesResultArray as $maxesRow ) {
+		if ( $row[0] == $maxesRow[0] ) {
+			$pdfDataTable[$playerCount - 1][13] = $maxesRow[4];
+			$pdfDataTable[$playerCount - 1][14] = $maxesRow[5];
+		}
+	}
+	
+	foreach ( $idsArray as $idsIndex => $id ) {
+		if ( $row[0] ==  $id ) {
+			$idsArray[$idsIndex] = -1;
+		}
+	}
 }
 
+foreach ( $idsArray as $idsIndex => $id ) {
+	foreach ( $maxesResultArray as $maxesIndex => $maxesRow ) {
+		if  ( $id != -1 && $maxesRow[0] == $id && ( $maxesRow[4] != 0 || $maxesRow[5] != 0 ) ) {
+			$pdfDataTable[] = array( $maxesRow[1], $maxesRow[2], $maxesRow[3], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, $maxesRow[4], $maxesRow[5] );
+		}
+	}
+}
 
-//var_dump( $pdfDataTable );
+function pdfReportComparator($row1, $row2) {
+	return $row1[1] > $row2[1];
+}
 
-$widths = array(30, 40, 20, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 10, 10);
+usort( $pdfDataTable, "pdfReportComparator" );
 
 $pdf = new PDF();
-// Column headings
-$header = array('Name', 'Vorname', 'HDSV nr.', '18', '17', '16', '15', '14', '13', '12', '11', '10', '9', '180er', '171er');
-// Data loading
 $pdf->SetFont('Arial','',9);
+$pdf->SetFillColor( 255, 255, 255 );
 $pdf->AddPage();
-$pdf->BasicTable($header,$pdfDataTable,$widths);
-//$pdf->AddPage();
-//$pdf->ImprovedTable($header,$data);
-//$pdf->AddPage();
-//$pdf->FancyTable($header,$data);
+$pdf->BasicTable($pdfDataTable);
 $pdf->Output();
 
 ?>
