e417c88e1edc3a4ced52dd01131097094e2f4a75
robbator@gmail.com <robbator@gmail.com@f5329d74-45e1-11de-b3c2-97ef4ed79301>
2013-05-13 19:33:40 +0000

    Turnieje: fixed encoding and gracz.php script.

--- trunk/Turnieje/gracz2.php
+++ trunk/Turnieje/gracz2.php
@@ -1,7 +1,7 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=ISO-8859-2">
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
 </head>
 <body>
 <script type="text/javascript">
@@ -15,12 +15,15 @@ function wstaw44(p_nr,p_fname,p_lname){
 <?php
 error_reporting(E_ERROR | E_PARSE);
 include_once("db.php");
+global $currentlang; //TODO get locale
+$currentlang = "german";
+include_once("language/lang-".$currentlang.".php");
 
 echo _Wybierzgracza;
 
 echo '<form method=post>';
 echo _Wpiszfragmentnazwygracza.' <input type="text" name="name" value="'.$_POST[name].'" /> <br><br>';
-echo '<input type="submit" value="'._szukaj.'" />';
+echo '<input type="submit" value="'._Wyslij.'" />';
 echo '<input type="hidden" value="'.$_REQUEST['cup_id'].'" />';
 echo '</form>';
 
@@ -29,11 +32,9 @@ if($_POST[name]!=''){
   $sql = "select id,fname,lname,player_number FROM "._DB_PREFIX."_players ".
    "where (fname LIKE '%$_POST[name]%' or lname LIKE '%$_POST[name]%') and cup_id=".$_REQUEST['cup_id'];
    
-    echo $sql;
 	$result = mysql_query($sql);
 
 while($row = mysql_fetch_row($result)){
-	var_dump($row);
 	echo "$row[1] $row[2] <a href=\"javascript:wstaw44('$row[3]','$row[1]','$row[2]');\">"._wstaw."</a><br>";
 }
 	
